2746f7fcba9e408038ae81e517deda4d07422ca7
Saullo Bretas Silva <saullo.silva303@gmail.com>
2022-06-12 00:01:16 -0300

    Kernel: Added file attributes (stat.h)

---
+++ kernel/api/posix/stat.h
@@ -0,0 +1,29 @@
+/**
+ * @file kernel/api/posix/stat.h
+ * @author Saullo Bretas Silva (saullo.silva303@gmail.com)
+ * @brief File attributes
+ * @version 0.1
+ * @date 2022-06-12
+ *
+ * @copyright Copyright (C) 2022 Saullo Bretas Silva
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+struct stat
+{
+    unsigned long st_size;
+};
