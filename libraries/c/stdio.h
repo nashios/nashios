d1f0e03a3fe55f6f6e4ebe5c41a912a1d10a018e
Saullo Bretas Silva <saullo.silva303@gmail.com>
2022-06-15 03:01:37 -0300

    LibC: Added vsnprintf() function

--- libraries/c/stdio.h
+++ libraries/c/stdio.h
@@ -12,6 +12,17 @@ typedef struct
     int unused;
 } FILE;
 
+enum vsn_flags
+{
+    VSN_ZEROPAD = 1,
+    VSN_SIGN = 2,
+    VSN_PLUS = 4,
+    VSN_SPACE = 8,
+    VSN_LEFT = 16,
+    VSN_SMALL = 32,
+    VSN_SPECIAL = 64
+};
+
 extern FILE *stderr;
 
 int fclose(FILE *);
@@ -24,3 +35,4 @@ long ftell(FILE *);
 size_t fwrite(const void *, size_t, size_t, FILE *);
 void setbuf(FILE *, char *);
 int vfprintf(FILE *, const char *, va_list);
+int vsnprintf(char *restrict s, size_t n, const char *restrict format, va_list ap);
