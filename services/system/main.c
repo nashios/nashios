b05eaa11719682740677e6ba5ddd529974b6596c
Saullo Bretas <saullo.bretas55@gmail.com>
2022-06-24 19:18:35 -0300

    System: Call mount(-a) process on main()

--- services/system/main.c
+++ services/system/main.c
@@ -1,5 +1,31 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void system_mount_all_fs()
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork()");
+        return;
+    }
+    else if (!pid)
+    {
+        if (execl("/bin/mount", "mount", "-a", NULL) < 0)
+        {
+            perror("execl()");
+            return;
+        }
+    }
+    else
+        wait(NULL);
+}
+
 int main()
 {
+    system_mount_all_fs();
+
     while (1)
         ;
 }
