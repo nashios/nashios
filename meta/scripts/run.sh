7c2ca096513e829245e2f3271d6985d6613a40c3
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-05-13 16:31:45 -0300

    Scripts: Added run script file

---
+++ meta/scripts/run.sh
@@ -0,0 +1,12 @@
+[ -z "$NASHIOS_QEMU_BIN" ] && NASHIOS_QEMU_BIN="qemu-system-i386"
+[ -z "$NASHIOS_QEMU_KERNEL" ] && NASHIOS_QEMU_KERNEL="kernel/kernel"
+[ -z "$NASHIOS_QEMU_RAM" ] && NASHIOS_QEMU_RAM="128M"
+[ -z "$NASHIOS_QEMU_CPU" ] && NASHIOS_QEMU_CPU="max"
+[ -z "$NASHIOS_QEMU_ARGS" ] && NASHIOS_QEMU_ARGS="
+    -kernel $NASHIOS_QEMU_KERNEL
+    -m $NASHIOS_QEMU_RAM
+    -cpu $NASHIOS_QEMU_CPU"
+
+[ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && NASHIOS_QEMU_VIRT="-enable-kvm"
+
+"$NASHIOS_QEMU_BIN" $NASHIOS_QEMU_ARGS $NASHIOS_QEMU_VIRT
