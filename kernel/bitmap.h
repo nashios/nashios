773143ae77905ae2a2d7e212bf2c2999de83b61f
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-05-13 23:14:58 -0300

    Kernel: Added bitmap page frame

---
+++ kernel/bitmap.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+uint32_t bitmap_first_free(uint32_t *address, size_t max);
+uint32_t bitmap_first_free_size(uint32_t *address, uint32_t max, size_t size);
+void bitmap_set(uint32_t *address, uint32_t bit);
+void bitmap_unset(uint32_t *address, uint32_t bit);
+bool bitmap_test(uint32_t *address, uint32_t bit);
