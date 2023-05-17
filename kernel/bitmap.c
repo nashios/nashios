30dd5112c916423267600f6ba85152bee60cc12f
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-05-17 20:54:27 -0300

    Kernel: Fixed bitmap find functions

--- kernel/bitmap.c
+++ kernel/bitmap.c
@@ -8,7 +8,7 @@ bool bitmap_test(uint32_t *address, uint32_t bit) { return address[bit / 32] & (
 
 uint32_t bitmap_first_free(uint32_t *address, size_t max)
 {
-    for (uint32_t i = 0; i < max; i++)
+    for (uint32_t i = 0; i < max / 32; i++)
     {
         if (address[i] == 0xFFFFFFFF)
             continue;
@@ -17,7 +17,7 @@ uint32_t bitmap_first_free(uint32_t *address, size_t max)
         {
             uint32_t bit = 1 << j;
             if (!(address[i] & bit))
-                return i * 4 * 8 + j;
+                return i * 32 + j;
         }
     }
 
@@ -29,20 +29,21 @@ uint32_t bitmap_first_free_size(uint32_t *address, uint32_t max, size_t size)
     if (size == 0)
         return -1;
 
-    for (uint32_t i = 0; i < max; i++)
+    if (size == 1)
+        return bitmap_first_free(address, max);
+
+    for (uint32_t i = 0; i < max / 32; i++)
     {
         if (address[i] == 0xFFFFFFFF)
             continue;
 
         for (uint32_t j = 0; j < 32; j++)
         {
-            auto bit = 1 << j;
+            uint32_t bit = 1 << j;
             if (address[i] & bit)
                 continue;
 
-            auto starting_bit = i * 32;
-            starting_bit += bit;
-
+            uint32_t starting_bit = i * 32 + bit;
             uint32_t free = 0;
             for (uint32_t count = 0; count <= size; count++)
             {
@@ -50,7 +51,7 @@ uint32_t bitmap_first_free_size(uint32_t *address, uint32_t max, size_t size)
                     free++;
 
                 if (free == size)
-                    return i * 4 * 8 + j;
+                    return i * 32 + j;
             }
         }
     }
