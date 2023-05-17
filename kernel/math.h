a005732088e012eff8caf1e7a64a02c294d3ae2e
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-05-17 09:54:48 -0300

    Kernel: Added MIN, MAX and ALIGN_DOWN and ALIGN_UP macros

--- kernel/math.h
+++ kernel/math.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #define DIV_ROUND_UP(number, divisor) (((number) + (divisor)-1) / (divisor))
+
+#define ALIGN_DOWN(base, size) ((base) & -((__typeof__(base))(size)))
+#define ALIGN_UP(base, size) ALIGN_DOWN((base) + (size)-1, (size))
+
+#define MIN(x, y)                                                                                                      \
+    ({                                                                                                                 \
+        typeof(x) p_x = (x);                                                                                           \
+        typeof(y) p_y = (y);                                                                                           \
+        (void)(&p_x == &p_y);                                                                                          \
+        p_x < p_y ? p_x : p_y;                                                                                         \
+    })
+
+#define MAX(x, y)                                                                                                      \
+    ({                                                                                                                 \
+        typeof(x) p_x = (x);                                                                                           \
+        typeof(y) p_y = (y);                                                                                           \
+        (void)(&p_x == &p_y);                                                                                          \
+        p_x > p_y ? p_x : p_y;                                                                                         \
+    })
