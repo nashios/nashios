1019d6b4bf14d77a7679d1f6858e3f1a1a08bfed
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-05-22 01:44:42 -0300

    LibC: Fixed crti.S and crtn.S files, removed unused code

--- libraries/c/crt0.c
+++ libraries/c/crt0.c
@@ -1,3 +1,10 @@
 extern int main(int argc, char **argv, char **envp);
 
-void _start(int argc, char **argv, char **envp) { main(argc, argv, envp); }
+void _start(int argc, char **argv, char **envp)
+{
+    while (1)
+        ;
+    main(argc, argv, envp);
+    while (1)
+        ;
+}
