aa2dc72a2a2878d09e18aacab7063ac7904e032f
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-05-25 00:29:05 -0300

    System: Added system_open() function

--- services/system/main.c
+++ services/system/main.c
@@ -1,1 +1,20 @@
-int main() { return 0; }
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void system_open(const char *path)
+{
+    pid_t pid = fork();
+    if (!pid)
+        execve(path, NULL, NULL);
+}
+
+int main()
+{
+    system_open("/bin/window");
+    system_open("/bin/desktop");
+
+    while (true)
+        ;
+}
