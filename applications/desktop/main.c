d235341d81cc9d05ef79836412d338f2561d1c65
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-05-29 17:40:17 -0300

    Desktop: Use libgui to create a window

--- applications/desktop/main.c
+++ applications/desktop/main.c
@@ -1,1 +1,12 @@
-int main() { return 0; }
+#include <gui/window.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct window *window = gui_create_window(1024, 768, 0, 0);
+    if (!window)
+        return EXIT_FAILURE;
+
+    while (true)
+        ;
+}
