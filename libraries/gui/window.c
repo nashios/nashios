4136f49cccd5d293ebc75eb910c600f9458ca149
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-05-31 18:26:56 -0300

    LibGui: Check if mq_close() returned error

--- libraries/gui/window.c
+++ libraries/gui/window.c
@@ -54,7 +54,7 @@ struct window *gui_create_window(int width, int height, int x, int y)
     if (mq_send(fd, (const char *)mq_gui, sizeof(struct mq_gui), 0) < 0)
         return NULL;
 
-    if (mq_close(fd))
+    if (mq_close(fd) < 0)
         return NULL;
 
     memset(&attr, 0x00, sizeof(struct mq_attr));
