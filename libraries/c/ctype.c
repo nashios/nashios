1cfccc9984d30a011a61c2f224fc71e57d0ea56b
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-01 16:48:21 -0300

    LibC: Implement ctype functions

--- libraries/c/ctype.c
+++ libraries/c/ctype.c
@@ -66,4 +66,99 @@ const char _ctype_[256] = {0,       _C,
                            _P,      _P,
                            _C};
 
-int isdigit(int c) { return (c >= '0') && (c <= '9'); }
+
+#undef isalnum
+int isalnum(int c)
+{
+    return ___isalnum(c);
+}
+
+#undef isalpha
+int isalpha(int c)
+{
+    return ___isalpha(c);
+}
+
+#undef iscntrl
+int iscntrl(int c)
+{
+    return ___iscntrl(c);
+}
+
+#undef isdigit
+int isdigit(int c)
+{
+    return ___isdigit(c);
+}
+
+#undef isxdigit
+int isxdigit(int c)
+{
+    return ___isxdigit(c);
+}
+
+#undef isspace
+int isspace(int c)
+{
+    return ___isspace(c);
+}
+
+#undef ispunct
+int ispunct(int c)
+{
+    return ___ispunct(c);
+}
+
+#undef isprint
+int isprint(int c)
+{
+    return ___isprint(c);
+}
+
+#undef isgraph
+int isgraph(int c)
+{
+    return ___isgraph(c);
+}
+
+#undef isupper
+int isupper(int c)
+{
+    return ___isupper(c);
+}
+
+#undef islower
+int islower(int c)
+{
+    return ___islower(c);
+}
+
+#undef isascii
+int isascii(int c)
+{
+    return ___isascii(c);
+}
+
+#undef isblank
+int isblank(int c)
+{
+    return ___isblank(c);
+}
+
+#undef toascii
+int toascii(int c)
+{
+    return ___toascii(c);
+}
+
+#undef tolower
+int tolower(int c)
+{
+    return ___tolower(c);
+}
+
+#undef toupper
+int toupper(int c)
+{
+    return ___toupper(c);
+}
