39923713e027079262626f011355c023055c868b
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-01 01:11:36 -0300

    LibC: Added fstat() function

--- libraries/c/sys/stat.c
+++ libraries/c/sys/stat.c
@@ -3,3 +3,6 @@
 
 _syscall2(stat, const char *, struct stat *);
 int stat(const char *path, struct stat *buf) { SYSCALL_RETURN(syscall_stat(path, buf)); }
+
+_syscall2(fstat, int, struct stat *);
+int fstat(int fildes, struct stat *buf) { SYSCALL_RETURN(syscall_fstat(fildes, buf)); }
