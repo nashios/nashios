f8486bb665949a793c37fee6f029c33af4e280da
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-05 23:25:48 -0300

    LibC: Added MB_CUR_MAX and MB_LEN_MAX macros

--- Libraries/C/stdlib.h
+++ Libraries/C/stdlib.h
@@ -7,6 +7,9 @@
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
 
+#define MB_CUR_MAX 4
+#define MB_LEN_MAX 16
+
 ___BEGIN_DECLS
 
 typedef struct
@@ -21,36 +24,70 @@ typedef struct
 {
 } lldiv_t;
 
+void _Exit(int);
+long a64l(const char *);
 void abort(void);
-int atexit(void (*func)(void));
+int abs(int);
+int atexit(void (*)(void));
+double atof(const char *);
 int atoi(const char *);
-char *getenv(const char *name);
-void *malloc(size_t size);
-void *calloc(size_t nelem, size_t elsize);
-void *realloc(void *ptr, size_t size);
-void free(void *ptr);
-void exit(int status);
-void _Exit(int status);
-int abs(int i);
-double atof(const char *str);
-long atol(const char *str);
-long long atoll(const char *nptr);
-void *bsearch(const void *key, const void *base, size_t nel, size_t width, int (*compar)(const void *, const void *));
-div_t div(int numer, int denom);
-long labs(long i);
-long long llabs(long long i);
-ldiv_t ldiv(long numer, long denom);
-lldiv_t lldiv(long long numer, long long denom);
-void qsort(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *));
+long atol(const char *);
+long long atoll(const char *);
+void *bsearch(const void *, const void *, size_t, size_t, int (*)(const void *, const void *));
+void *calloc(size_t, size_t);
+div_t div(int, int);
+double drand48(void);
+double erand48(unsigned short[3]);
+void exit(int);
+void free(void *);
+char *getenv(const char *);
+int getsubopt(char **, char *const *, char **);
+int grantpt(int);
+char *initstate(unsigned, char *, size_t);
+long jrand48(unsigned short[3]);
+char *l64a(long);
+long labs(long);
+void lcong48(unsigned short[7]);
+ldiv_t ldiv(long, long);
+long long llabs(long long);
+lldiv_t lldiv(long long, long long);
+long lrand48(void);
+void *malloc(size_t);
+int mblen(const char *, size_t);
+size_t mbstowcs(wchar_t *, const char *, size_t);
+int mbtowc(wchar_t *, const char *, size_t);
+char *mkdtemp(char *);
+int mkstemp(char *);
+long mrand48(void);
+long nrand48(unsigned short[3]);
+int posix_memalign(void **, size_t, size_t);
+int posix_openpt(int);
+char *ptsname(int);
+int putenv(char *);
+void qsort(void *, size_t, size_t, int (*)(const void *, const void *));
 int rand(void);
-int rand_r(unsigned *seed);
-void srand(unsigned seed);
-double strtod(const char *str, char **endptr);
-float strtof(const char *nptr, char **endptr);
-long int strtol(const char *str, char **endptr, int base);
-long double strtold(const char *nptr, char **endptr);
-unsigned long int strtoul(const char *str, char **endptr, int base);
-unsigned long long strtoull(const char *str, char **endptr, int base);
-int system(const char *command);
+int rand_r(unsigned *);
+long random(void);
+void *realloc(void *, size_t);
+char *realpath(const char *, char *);
+unsigned short *seed48(unsigned short[3]);
+int setenv(const char *, const char *, int);
+void setkey(const char *);
+char *setstate(char *);
+void srand(unsigned);
+void srand48(long);
+void srandom(unsigned);
+double strtod(const char *, char **);
+float strtof(const char *, char **);
+long strtol(const char *, char **, int);
+long double strtold(const char *, char **);
+long long strtoll(const char *, char **, int);
+unsigned long strtoul(const char *, char **, int);
+unsigned long long strtoull(const char *, char **, int);
+int system(const char *);
+int unlockpt(int);
+int unsetenv(const char *);
+size_t wcstombs(char *, const wchar_t *, size_t);
+int wctomb(char *, wchar_t);
 
 ___END_DECLS
