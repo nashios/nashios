4e78197c77430b990264fd33b46a2e26debb6e85
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-06 17:23:54 -0300

    Libraries: Removed gfx and gui libraries

--- Libraries/gfx/bitmap.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <gfx/graphic.h>
-
-char *gfx_bitmap_open(const char *path);
-void gfx_bitmap_draw(struct graphic *graphic, char *bitmap, int x, int y);
--- Libraries/gui/event_loop.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <gui/window.h>
-
-int gui_event_loop(struct window *window);
