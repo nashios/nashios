a35a8889cf0f1b005545dc5366c4f08e300f6683
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-06 20:09:37 -0300

    LibC: Print to stderr and abort() when __assert_fail()

--- Libraries/C/assert.cpp
+++ Libraries/C/assert.cpp
@@ -1,6 +1,16 @@
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 extern "C"
 {
-    void __assert_fail(const char *assertion, const char *file, unsigned int line, const char *function) {}
+    void __assert_fail(const char *assertion, const char *file, unsigned int line, const char *function)
+    {
+        fprintf(stderr, "Assertion: Message = %s\n", assertion);
+        fprintf(stderr, "Assertion: File = %s\n", file);
+        fprintf(stderr, "Assertion: Line = %d\n", line);
+        fprintf(stderr, "Assertion: Function = %s\n", function);
+
+        abort();
+    }
 }
