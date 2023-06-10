7589c7c265d2e7ccfb43c6c353abace0e05de127
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-09 23:59:50 -0300

    LibC: Added sbrk() and brk() support

---
+++ libraries/c/unistd.c
@@ -0,0 +1,11 @@
+#include <sys/syscall.h>
+#include <unistd.h>
+
+int brk(void *addr) { return syscall(__NR_brk, addr); }
+
+void *sbrk(intptr_t increment)
+{
+    void *value = (void *)brk(0);
+    brk(value + increment);
+    return value;
+}
--- libraries/c/unistd.h
+++ libraries/c/unistd.h
@@ -1,6 +1,7 @@
 #ifndef _UNISTD_H
 #define _UNISTD_H
 
+#include <stdint.h>
 #include <sys/types.h>
 
 #ifdef __cplusplus
@@ -12,6 +13,27 @@ extern "C"
     int execve(const char *, char *const[], char *const[]);
     int execvp(const char *, char *const[]);
     pid_t fork(void);
+
+    /**
+     * @brief Change the data segment size
+     *
+     * @note See more at: https://man7.org/linux/man-pages/man2/brk.2.html
+     *
+     * @param addr The new end of the data segment
+     * @return int On sucess, returns zero. On error, -1 is returned, and errno is set to ENOMEM.
+     */
+    int brk(void *addr);
+
+    /**
+     * @brief Increase program data space
+     * 
+     * @note See more at: https://man7.org/linux/man-pages/man2/brk.2.html
+     *
+     * @param increment The number of bytes to increase the data segment by, 0 to return the current location of the
+     * program break
+     * @return void* On success, returns the previous program break. On error, (void*) -1 is returned, and errno is set
+     */
+    void *sbrk(intptr_t increment);
 
 #ifdef __cplusplus
 }
