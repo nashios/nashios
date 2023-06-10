2566264f4823720f615795ef91a587e9e48b73a9
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-10 09:50:42 -0300

    LibC: Check for open() args

--- libraries/c/fcntl.c
+++ libraries/c/fcntl.c
@@ -1,4 +1,18 @@
 #include <fcntl.h>
+#include <stdarg.h>
 #include <sys/syscall.h>
+#include <sys/types.h>
 
-int open(const char *path, int oflag, ...) { return syscall(__NR_open, path, oflag); }
+int open(const char *path, int oflag, ...)
+{
+    mode_t mode = 0;
+    if (oflag & O_CREAT)
+    {
+        va_list args;
+        va_start(args, oflag);
+        mode = va_arg(args, mode_t);
+        va_end(args);
+    }
+
+    return syscall(__NR_open, path, oflag, mode);
+}
--- libraries/c/sys/ioctl.c
+++ libraries/c/sys/ioctl.c
@@ -6,7 +6,7 @@ int ioctl(int fd, unsigned long request, ...)
 {
     va_list ap;
     va_start(ap, request);
-    void* arg = va_arg(ap, void*);
+    void *arg = va_arg(ap, void *);
     va_end(ap);
 
     return syscall(__NR_ioctl, fd, request, arg);
