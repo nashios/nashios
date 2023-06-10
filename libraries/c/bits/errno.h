f6627b683f836e0aa17409ca631f5fe2256b4d49
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-10 00:39:43 -0300

    LibC: Use errno definitions from kernel/api

---
+++ libraries/c/bits/errno.h
@@ -0,0 +1,6 @@
+#ifndef _BITS_ERRNO_H
+#define _BITS_ERRNO_H
+
+#include <kernel/api/posix/errno.h>
+
+#endif
--- libraries/c/errno.h
+++ libraries/c/errno.h
@@ -1,6 +1,8 @@
 #ifndef _ERRNO_H
 #define _ERRNO_H
 
+#include <bits/errno.h>
+
 extern int errno;
 
 #endif
