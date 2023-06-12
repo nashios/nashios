cfcc28b5b927aa0cc67deaa24be57e5268949d50
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-12 03:55:13 -0300

    St: Added debug support

---
+++ st/debug.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#if defined(KERNEL)
+#else
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#endif
+
+void dbgln(const char *format, ...)
+{
+#if defined(KERNEL)
+#else
+    static int fd = -1;
+    if (fd == -1)
+    {
+        fd = open("/dev/debug", O_WRONLY);
+        if (fd == -1)
+            return;
+    }
+
+    va_list args1;
+    va_start(args1, format);
+
+    va_list args2;
+    va_copy(args2, args1);
+
+    int length = vsnprintf(NULL, 0, format, args1);
+    va_end(args1);
+
+    if (length < 0)
+        return;
+
+    char *buffer = (char *)calloc(length + 1, sizeof(char));
+    if (buffer == NULL)
+        return;
+
+    length = vsnprintf(buffer, length + 1, format, args2);
+    if (length < 0)
+        return;
+    va_end(args2);
+
+    if (write(fd, buffer, length) < 0)
+        return;
+
+    free(buffer);
+
+#endif
+}
+
+#define dbgln_if(condition, format, ...)                                                                               \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if ((condition))                                                                                               \
+            dbgln(format, ##__VA_ARGS__);                                                                              \
+    } while (0);
