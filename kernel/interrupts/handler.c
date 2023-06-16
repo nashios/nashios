315e5297a52f7bde1febadcb0db9095d0cf5588c
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-16 11:15:30 -0300

    Kernel: Added default isr_handler() for exceptions

--- kernel/interrupts/handler.c
+++ kernel/interrupts/handler.c
@@ -1,6 +1,7 @@
 #include <kernel/interrupts/handler.h>
 #include <kernel/lib/stdio.h>
 #include <kernel/lib/stdlib.h>
+#include <kernel/panic.h>
 #include <st/dlist.h>
 
 struct itr_handler
@@ -10,6 +11,38 @@ struct itr_handler
 };
 
 static struct dlist_head s_itr_handlers[256];
+static const char *s_isr_messages[32] = {"Division Error",
+                                         "Debug",
+                                         "Non-maskable Interrupt",
+                                         "Breakpoint",
+                                         "Overflow",
+                                         "Bound Ranged Exceeded",
+                                         "Invalid Opcode",
+                                         "Device Not Available",
+                                         "Double fault",
+                                         "Coprocessor Segment Overrun",
+                                         "Invalid TSS",
+                                         "Segment Not Present",
+                                         "Stack-Segment Fault",
+                                         "General Protection Fault",
+                                         "Page Fault",
+                                         "Reserved",
+                                         "x87 Floating-Point Exception",
+                                         "Alignment Check",
+                                         "Machine Check",
+                                         "SIMD Floating-Point Exception",
+                                         "Virtualization Exception",
+                                         "Control Protection Exception",
+                                         "Reserved",
+                                         "Reserved",
+                                         "Reserved",
+                                         "Reserved",
+                                         "Reserved",
+                                         "Reserved",
+                                         "Hypervisor Injection Exception",
+                                         "VMM Communication Exception",
+                                         "Security Exception",
+                                         "Reserved"};
 
 void itr_set_handler(int number, itr_handler_t handler)
 {
@@ -38,7 +71,7 @@ void itr_unset_handler(int number, itr_handler_t handler)
 
 void itr_handler(struct itr_registers *registers)
 {
-    int number = registers->number;
+    int number = registers->number & 0xFF;
     struct dlist_head *list = &s_itr_handlers[number];
     if (dlist_empty(list))
     {
@@ -47,17 +80,49 @@ void itr_handler(struct itr_registers *registers)
     }
 
     struct itr_handler *itr;
-    dlist_for_each_entry(itr, list, list)
+    dlist_for_each_entry_reverse(itr, list, list)
     {
         if (itr->handler(registers) == ITR_STOP)
             break;
     }
 }
 
+bool isr_handler(struct itr_registers *registers)
+{
+    printf("ISR: Exception number = %d, message = %s\n", registers->number, s_isr_messages[registers->number]);
+    PANIC("Exception");
+    return ITR_STOP;
+}
+
 void itr_init()
 {
     for (int i = 0; i < 256; i++)
         dlist_head_init(&s_itr_handlers[i]);
+
+    itr_set_handler(0, isr_handler);
+    itr_set_handler(1, isr_handler);
+    itr_set_handler(2, isr_handler);
+    itr_set_handler(3, isr_handler);
+    itr_set_handler(4, isr_handler);
+    itr_set_handler(5, isr_handler);
+    itr_set_handler(6, isr_handler);
+    itr_set_handler(7, isr_handler);
+    itr_set_handler(8, isr_handler);
+    itr_set_handler(9, isr_handler);
+    itr_set_handler(10, isr_handler);
+    itr_set_handler(11, isr_handler);
+    itr_set_handler(12, isr_handler);
+    itr_set_handler(13, isr_handler);
+    itr_set_handler(14, isr_handler);
+    itr_set_handler(16, isr_handler);
+    itr_set_handler(17, isr_handler);
+    itr_set_handler(18, isr_handler);
+    itr_set_handler(19, isr_handler);
+    itr_set_handler(20, isr_handler);
+    itr_set_handler(21, isr_handler);
+    itr_set_handler(28, isr_handler);
+    itr_set_handler(29, isr_handler);
+    itr_set_handler(30, isr_handler);
 
     printf("ITR: Initialized\n");
 }
