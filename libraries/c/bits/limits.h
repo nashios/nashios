9146d82f22eb5f66b1a4491d64b3647dacd7bbec
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-06-16 14:11:43 -0300

    LibC: Added SHRT/USHRT min and max macros

--- libraries/c/bits/limits.h
+++ libraries/c/bits/limits.h
@@ -3,6 +3,15 @@
 
 #include <kernel/api/posix/limits.h>
 
+#define SHRT_MIN (-SHRT_MAX - 1)
+#define SHRT_MAX __SHRT_MAX__
+
+#if __SHRT_MAX__ == __INT_MAX__
+#define USHRT_MAX (SHRT_MAX * 2U + 1U)
+#else
+#define USHRT_MAX (SHRT_MAX * 2 + 1)
+#endif
+
 #define ULONG_MAX ((unsigned long)(~0L))
 
 #endif
