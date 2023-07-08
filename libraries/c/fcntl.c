f6f7d7af808e6fc156e93d78e2b93780f05c19b7
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-07-08 15:44:29 -0300

    fix [libc]: use void* type on fcntl() args

--- libraries/c/fcntl.c
+++ libraries/c/fcntl.c
@@ -1,3 +1,27 @@
+/**
+ * @file libraries/c/fcntl.c
+ * @author Saullo Bretas Silva (saullo.silva55@outlook.com)
+ * @brief
+ * @version 0.0.1
+ * @date 2023-07-08
+ *
+ * @copyright Nashi Operating System
+ * Copyright (C) 2023 Saullo Bretas Silva <saullo.silva55@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 #include <fcntl.h>
 #include <stdarg.h>
 #include <sys/syscall.h>
@@ -21,7 +45,7 @@ int fcntl(int fildes, int cmd, ...)
 {
     va_list ap;
     va_start(ap, cmd);
-    unsigned long args = va_arg(ap, unsigned long);
+    void *args = va_arg(ap, void *);
     va_end(ap);
 
     return syscall(__NR_fcntl, fildes, cmd, args);
