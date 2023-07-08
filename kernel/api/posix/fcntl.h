a512dc718d9c39e3dfc923479cbe7308b26d2c26
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-07-07 23:58:46 -0300

    feat [kernel]: added more fcntl definitions

--- kernel/api/posix/fcntl.h
+++ kernel/api/posix/fcntl.h
@@ -9,3 +9,16 @@
 #define O_TRUNC 00001000
 #define O_APPEND 00002000
 #define O_NONBLOCK 00004000
+
+#define F_DUPFD 0
+#define F_GETFD 1
+#define F_SETFD 2
+#define F_GETFL 3
+#define F_SETFL 4
+#define F_GETLK 5
+#define F_SETLK 6
+#define F_SETLKW 7
+#define F_SETOWN 8
+#define F_GETOWN 9
+#define F_SETSIG 10
+#define F_GETSIG 11
