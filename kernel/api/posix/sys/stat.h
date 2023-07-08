7aff30f5b5c3d21fa22300d056e36d8a8e9b7066
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-07-07 23:58:31 -0300

    feat [kernel]: added more struct stat members

--- kernel/api/posix/sys/stat.h
+++ kernel/api/posix/sys/stat.h
@@ -23,8 +23,16 @@ struct stat
     unsigned long st_ino;
     unsigned short st_mode;
     unsigned short st_nlink;
+    unsigned short st_uid;
+    unsigned short st_gid;
+    unsigned long st_rdev;
     unsigned long st_size;
     unsigned long st_blksize;
     unsigned long st_blocks;
+    unsigned long st_atime;
+    unsigned long st_atime_nsec;
     unsigned long st_mtime;
+    unsigned long st_mtime_nsec;
+    unsigned long st_ctime;
+    unsigned long st_ctime_nsec;
 };
